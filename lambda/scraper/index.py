9ae5638017d345189875890f1f4757d520b9e2dc
unknown <alyu@ucsd.edu>
2022-04-19 14:47:18 -0700

    update requirements.txt

--- lambda/scraper/index.py
+++ lambda/scraper/index.py
@@ -8,12 +8,11 @@ output_bucket = client.Bucket(os.environ['OUTPUT_BUCKET'])
 
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
-    
-    try:
-        print(event['url'])
-        url = event['url']
-    except:
-        url = 'test2.html'
 
-    # Write to output bucket
-    output_bucket.put_object(Body=open('test.html', 'rb'), Key=url)
+    print(event['url'])
+    url = event['url']
+    r = requests.get(url)
+    
+    fname = url.replace('http://', '')
+    fname = fname.replace('https://', '')
+    output_bucket.put_object(Body=r.content, Key=fname+ '.html')
