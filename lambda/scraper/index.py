1bdff6053fff43ca36295d6758fed74565eac925
unknown <alyu@ucsd.edu>
2022-04-19 14:55:45 -0700

    update fn code

--- lambda/scraper/index.py
+++ lambda/scraper/index.py
@@ -2,17 +2,23 @@ import json
 import boto3
 import requests
 import os
+import undetected_chromedriver as uc
+driver = uc.Chrome()
+
 
 client = boto3.resource('s3')
 output_bucket = client.Bucket(os.environ['OUTPUT_BUCKET'])
 
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
+    
 
     print(event['url'])
     url = event['url']
     r = requests.get(url)
+    driver.get('https://nowsecure.nl')
     
     fname = url.replace('http://', '')
     fname = fname.replace('https://', '')
-    output_bucket.put_object(Body=r.content, Key=fname+ '.html')
+    fname = fname.replace('/', '')
+    output_bucket.put_object(Body=driver.page_source, Key=fname+ '.html')
