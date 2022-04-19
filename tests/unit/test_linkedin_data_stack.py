e8817c3be267f74bae4a46f3e8e87a50f9313ca3
unknown <alyu@ucsd.edu>
2022-04-19 12:00:32 -0700

    cdk init

---
+++ tests/unit/test_linkedin_data_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from linkedin_data.linkedin_data_stack import LinkedinDataStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in linkedin_data/linkedin_data_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = LinkedinDataStack(app, "linkedin-data")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
